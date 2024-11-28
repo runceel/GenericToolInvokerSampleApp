171bd8b592a5119cb55f0b5dde992ce3de2a8ed3
Kazuki Ota <kaota@microsoft.com>
2024-11-28 15:45:02 +0900

    プロジェクト ファイルを追加します。

---
+++ GenericToolInvokerSampleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// ツール呼び出しクラス使用例
+// 理想的にはアプリ全体で DI コンテナを 1 個持ちたい
+var invoker = new ToolInvoker(new SomeClass());
+Console.WriteLine(invoker.Run<Tool1>("arg1"));
+Console.WriteLine(invoker.Run<Tool2>("arg2"));
---
+++ GenericToolInvokerSampleApp/Tools.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+// ツールの基本クラス
+abstract class ToolBase
+{
+    public abstract int Run(string arg);
+}
+
+// 各種ツールの実装
+class Tool1 : ToolBase
+{
+    public override int Run(string arg)
+    {
+        Console.WriteLine($"Tool1 is running with {arg}.");
+        return 1;
+    }
+}
+
+class Tool2 : ToolBase
+{
+    private readonly SomeClass someClass;
+
+    public Tool2(SomeClass someClass)
+    {
+        this.someClass = someClass;
+    }
+
+    public override int Run(string arg)
+    {
+        Console.WriteLine($"Tool2 is running with {arg}. {someClass}.");
+        return 2;
+    }
+}
+
+// 特定のツールには必要なクラス
+class SomeClass { }
+
+// ツールを呼ぶ汎用クラス
+class ToolInvoker
+{
+    private IServiceProvider serviceProvider;
+    public ToolInvoker(SomeClass someClass)
+    {
+        serviceProvider = new ServiceCollection()
+            .AddSingleton<SomeClass>(someClass)
+            .AddSingleton<Tool1>()
+            .AddSingleton<Tool2>()
+            .BuildServiceProvider();
+    }
+
+    public int Run<T>(string arg) where T : ToolBase
+    {
+        var tool = serviceProvider.GetRequiredService<T>();
+        return tool.Run(arg);
+    }
+}
